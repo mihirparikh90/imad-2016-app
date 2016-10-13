769047183b63ad7efd064ca4688f9247a1d84488
mihirparikh90 <mihirparikh90@gmail.com>
2016-10-13 22:14:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/Article-One',function (req,res){
+    res.send('Article One Requested');
+});
+
+app.get('/Article-Two',function (req,res){
+    res.send('Article Two Requested');
+});
+
+app.get('/Article-Three',function (req,res){
+    res.send('Article Three Requested');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
