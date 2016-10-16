bd738e6261b2aafb3ace5ec8a92e9b19cd3402e9
mihirparikh90 <mihirparikh90@gmail.com>
2016-10-16 22:36:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 
 var element = document.getElementById('mainText');
 element.innerHTML = 'New Value';
+
+//Move image
+var img = document.getElementById('mihir');
+
+img.onclick = function () {
+    img.style.marginLeft = '100px';
+};
